2fbc495bdc5689757698ad61a36308b1f261552a
enjarai <enjarai@trans.fish>
2025-07-23 22:36:10 +0200

    lets push this

--- flake.nix
+++ flake.nix
@@ -2,27 +2,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
-    crane.url = "github:ipetkov/crane";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ (import rust-overlay) ];
         };
-        craneLib = (crane.mkLib pkgs).overrideToolchain 
-          (p: p.rust-bin.nightly.latest.default);
       in {
         # devShells.default = import ./shell.nix { inherit pkgs; };
-        packages.default = pkgs.callPackage ./package.nix {
-          inherit craneLib;
-        };
+        packages.default = pkgs.callPackage ./package.nix { };
       }
     ) // {
       nixosModule = import ./module.nix { 
--- package.nix
+++ package.nix
@@ -1,9 +1,29 @@
-{ craneLib, lib, rustPlatform, openssl, pkgconf }:
+{ lib, makeRustPlatform, rust-bin, openssl, pkgconf }:
 
-craneLib.buildPackage {
-  src = craneLib.cleanCargoSource ./.;
+# craneLib.buildPackage {
+#   src = craneLib.cleanCargoSource ./.;
+
+#   buildInputs = [
+#     openssl
+#   ];
+
+#   nativeBuildInputs = [
+#     pkgconf
+#   ];
+
+#   doCheck = false;
+# }
+(makeRustPlatform {
+  cargo = rust-bin.nightly.latest.default;
+  rustc = rust-bin.nightly.latest.default;
+}).buildRustPackage rec {
+  pname = "mental-instability-bot";
+  version = "0.1.0";
+
+  src = ./.;
 
   buildInputs = [
+    # alsa-lib
     openssl
   ];
 
@@ -11,31 +31,17 @@ craneLib.buildPackage {
     pkgconf
   ];
 
+  cargoLock = {
+    lockFile = src + /Cargo.lock;
+    outputHashes = {
+      "poise-0.6.1" = "sha256-j72ha9Sn/A8F/PGoPDCAF8ThlPuigUWpK1GoFJSvPxg=";
+      "serenity-0.12.1" = "sha256-g2/5dP8gDwYybhbG9iD59xwOKaKKDDlkYEerlhDYd9A=";
+    };
+  };
   doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://github.com/enjarai/mental-instability-bot";
+    description = "The bot of the discord";
+  };
 }
-# rustPlatform.buildRustPackage rec {
-#   pname = "mental-instability-bot";
-#   version = "0.1.0";
-
-#   src = ./.;
-
-#   buildInputs = [
-#     # alsa-lib
-#     # openssl
-#   ];
-
-#   nativeBuildInputs = [
-#     pkgconf
-#   ];
-
-#   cargoLock = {
-#     lockFile = src + /Cargo.lock;
-#     allowBuiltinFetchGit = true;
-#   };
-#   doCheck = false;
-
-#   meta = with lib; {
-#     homepage = "https://github.com/enjarai/mental-instability-bot";
-#     description = "The bot of the discord";
-#   };
-# }
