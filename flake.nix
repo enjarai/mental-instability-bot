fbe5da62c9187018cbf77b2f1b56fb520b244153
enjarai <38552516+enjarai@users.noreply.github.com>
2025-07-30 22:58:36 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
